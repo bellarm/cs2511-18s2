de7245f283ed86095b5dcdcae8b46fe235086cde
Bella Mangunsong <rachelitabella@rocketmail.com>
2018-07-29 12:39:48 +1000

    add a bit of comments

--- src/tut01/HelloWorldGreeting.java
+++ src/tut01/HelloWorldGreeting.java
@@ -9,13 +9,25 @@ public class HelloWorldGreeting {
 	}
 	
 	public static void main(String[] args) {
+		// Create an instance of this class.
 		HelloWorldGreeting hello1 = new HelloWorldGreeting();
+		// Change the greeting field.
 		hello1.setGreeting("COMP2511 is cool");
 		System.out.println(hello1.greeting);
 	
+		// Create another object.
 		HelloWorldGreeting hello2 = new HelloWorldGreeting();
 		hello2.setGreeting("hello2");
 		System.out.println(hello2.greeting);
+
+		// The previous object still has the old string.
+		System.out.println(hello1.greeting);
+		
+		/* It's important to note that when you create a new object,
+		 * a new chunk of memory will be allocated for it, and the field
+		 * (ie. the variable `greeting`) is different for every instance
+		 * of the class.
+		 */
 	}
 	
 }
